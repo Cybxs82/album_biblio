609ea23d9e42e2e1b5ecaec812ff02eb4311ed50
Cybxs82 <leonardo.dom82@gmail.com>
2025-11-18 08:24:16 -0700

    Añadimiento Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
+import 'package:album_biblio/views/PaginaLogin.dart';
 import 'package:flutter/material.dart';
 import 'views/album_lista.dart';
 import 'package:provider/provider.dart';
 import 'package:album_biblio/model/album_biblio.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:album_biblio/firebase_options.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
 
-void main() {
-  runApp(
-    ChangeNotifierProvider(create: (_) => AlbumBiblio(), child: const MyApp()),
-  );
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp
+  (options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseUIAuth.configureProviders([
+    EmailAuthProvider(),
+    GoogleProvider(clientId: DefaultFirebaseOptions.currentPlatform.appId),
+  ]);
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -15,10 +25,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Album DEMO',
-      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.blue),
-      home: AlbumLista(),
+      title: 'Ejemplo firebase_auth',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const PaginaLogin(),
     );
   }
+  
 }
